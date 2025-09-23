0268cf1b41ba888368bc0f9b934e46f3a51a3ede
Hemang Vyas <vyashemang563@gmail.com>
2025-09-23 18:58:12 +0530

    API - Place Order

---
+++ src/main/java/me/vyashemang/spring_redis_restaurant/repository/OrderRepository.java
@@ -0,0 +1,4 @@
+package me.vyashemang.spring_redis_restaurant.repository;
+
+public interface OrderRepository {
+}
---
+++ src/main/java/me/vyashemang/spring_redis_restaurant/service/UserService.java
@@ -0,0 +1,4 @@
+package me.vyashemang.spring_redis_restaurant.service;
+
+public class UserService {
+}
